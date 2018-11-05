33b1986e6d21118df010e1bea8b1e4b51a874a85
steven ciasullo <sjciasullo@gmail.com>
2018-11-05 15:46:50 -0500

    unity roll a ball buildable project

---
+++ Assets/Editor/ScriptBatch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.IO;
+using System;
+
+
+public class ScriptBatch : MonoBehaviour {
+    public static void MyBuild ()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        List<string> enabledScenePathnames = new List<string>();
+
+        foreach (var buildSettingScene in EditorBuildSettings.scenes)
+        {
+            enabledScenePathnames.Add(buildSettingScene.path);
+        }
+
+        buildPlayerOptions.scenes = enabledScenePathnames.ToArray();
+        //buildPlayerOptions.locationPathName = Path.Combine("Build", DateTime.Now.ToString("_MM-dd-yyyy HH--mm--ss"));
+        buildPlayerOptions.locationPathName = "Build";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+        
+        if( summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Hooray! We conquered the universe!!!!! xD xD xD xD");
+        } else
+        {
+            Debug.Log("Entropy conquered us instead... Dx Dx Dx");
+        }
+    }
+}
